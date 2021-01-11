28d78bd171521593a9ab395141ff71abc9a5c2b2
sudhir thakare <sudhirtha7972@gmail.com>
2021-01-11 19:10:14 +0530

    project added

---
+++ configuration.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app=Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://{}:{}@{}/{}'.format('zhdiznduxcemmt','85325826bd2a1f8780d4ec4e3082b615a2fe1f0eac82929e0a5daa60710eb8f8
+','ec2-52-204-113-104.compute-1.amazonaws.com','d5m6rkbonukbu6')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '1238sadh1234390823kjhdJKA*(@E$^$'
+
+db=SQLAlchemy(app)
---
+++ maincontroller.py
@@ -0,0 +1,9 @@
+from hotelcontroller import *
+from roomcontroller import *
+from accountcontroller import *
+from menucontroller import *
+from customers_controller import *
+from logincontroller import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
